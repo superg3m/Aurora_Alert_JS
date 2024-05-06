4a6b579f355f61dfc0a6c6cfccd65f12bf0a8838
Jovanni D <80374076+superg3m@users.noreply.github.com>
2024-05-06 11:01:20 -0400

    Init

---
+++ index.js
@@ -0,0 +1,27 @@
+token = require("./config.json").TOKEN;
+const { Client, IntentsBitField } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+});
+
+client.on("ready", (c) => {
+  console.log(`✅ ${c.user.tag} is online.`);
+});
+
+client.on("messageCreate", (message) => {
+  if (message.author.bot) {
+    return;
+  }
+
+  if (message.content === "hello") {
+    message.reply("hello");
+  }
+});
+
+client.login(token);
